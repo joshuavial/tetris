27b027e32b4eb3079eb0a39079481ab805ffb296
Jason Veng <jasonveng@outlook.com>
2016-01-27 09:34:49 +1300

    got board to render properly to the screen size

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -2,14 +2,62 @@ import React, { Component } from 'react'
 
 console.log('working')
 
-class App extends Component {
+const width = window.innerWidth
+const height = window.innerHeight
 
+function getBoard (w, h) {
+  const board = []
+  for (let i = 0; i < h; i++) {
+    board[i] = []
+    for (let j = 0; j < w; j++) {
+      board[i][j] = 0
+    }
+  }
+  return board
+}
+
+const divStyle = {
+  width: width,
+  height: height
+}
+
+class App extends Component {
   constructor (props) {
-    super(props) 
+    super(props)
+    const width = window.innerWidth
+    const height = window.innerHeight
+    const cellWidth = 24
+    const widthCount = Math.floor(width / cellWidth)
+    const heightCount = Math.floor(height / cellWidth)
+    
+    this.state = {
+      board: getBoard(widthCount, heightCount)
+    }
   }
   
   render () {
-    return <div className='amazed'>Put maze here</div>
+    const board = this.state.board
+    const elements = board.map(function (row) {
+      return (
+        <tr className='row'>
+          {
+            row.map(function (cell) {
+              return <td></td>
+            })
+          }
+        </tr>
+      )    
+    })
+    console.log(board)
+    return <div style={divStyle}>
+      <table>
+        <tbody>
+          {
+            elements
+          }
+        </tbody>
+      </table>
+    </div>
   } 
 }
 
